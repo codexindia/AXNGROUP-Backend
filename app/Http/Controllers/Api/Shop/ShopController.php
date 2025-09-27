92659d1ca756aa3f718150119292bc4227ba426d
codexindia <sudiptobain8@gmail.com>
2025-09-27 22:41:19 +0530

    fix: Comment out eager loading and data transformation in getByAgent method for debugging

--- app/Http/Controllers/Api/Shop/ShopController.php
+++ app/Http/Controllers/Api/Shop/ShopController.php
@@ -70,14 +70,14 @@ class ShopController extends Controller
             })
           
             ->orderBy('created_at', 'desc')
-            ->with(['onboardingSheetData:phone,qr_trx,referral'])
-            ->paginate(20)
-            ->through(function ($shop) {
-                $shop['qr_trx'] = $shop->onboardingSheetData ? $shop->onboardingSheetData->qr_trx : 0;
-                $shop['s_referral'] = $shop->onboardingSheetData ? $shop->onboardingSheetData->referral : null;
-                unset($shop->onboardingSheetData);
-                return $shop;
-            });
+           // ->with(['onboardingSheetData:phone,qr_trx,referral'])
+            ->paginate(20);
+            // ->through(function ($shop) {
+            //     $shop['qr_trx'] = $shop->onboardingSheetData ? $shop->onboardingSheetData->qr_trx : 0;
+            //     $shop['s_referral'] = $shop->onboardingSheetData ? $shop->onboardingSheetData->referral : null;
+            //     unset($shop->onboardingSheetData);
+            //     return $shop;
+            // });
 
         return response()->json([
             'success' => true,
