8c28c57f65c43e0f2b0836c5bd4c23d6e1d1f621
codexindia <sudiptobain8@gmail.com>
2025-10-29 22:03:07 +0530

    fix: Increase maximum limit for per_page parameter in onboarding history request validation

--- app/Http/Controllers/Api/Shop/ShopController.php
+++ app/Http/Controllers/Api/Shop/ShopController.php
@@ -158,7 +158,7 @@ class ShopController extends Controller
             'date_from' => 'nullable|date',
             'date_to' => 'nullable|date|after_or_equal:date_from',
             'search' => 'nullable|string|max:255',
-            'per_page' => 'nullable|integer|min:1|max:100'
+            'per_page' => 'nullable|integer|min:1|max:10000'
         ]);
 
         if ($validator->fails()) {
